020a223d893dc16dfcb0311e4986d4257f032348
Nina Cummings <jaderiver64@gmail.com>
2021-01-22 17:14:48 -0600

    working on my fetch

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,39 @@ var searchBtn = document.getElementById("btn");
 
 var searchHandler= function(event) {
     event.preventDefault();
+    var resultEl = document.getElementById("search-result-container");
     var inputContent = document.getElementById("citySelection").value;
-    document.getElementById("search-result-container").innerHTML = "Hello World: "+ inputContent;
+    if(inputContent){
+        getWeather(inputContent);
+        
+    
   };
+};
 searchBtn.addEventListener("click", searchHandler);
+
+var getWeather = function(input){
+    console.log(input);
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=33.441792&lon=-94.037689&exclude=hourly,daily&appid=3812ea6836536b0581712ffd66f54fa5";
+
+    // make a get request to url
+    fetch(apiUrl).then(function(response) {
+        // request was successful
+        if (response.ok) {
+            response.json().then(function(data) {
+                displayWeather(data, input);
+            });
+        } else {
+            alert("Error: " + response.statusText);
+        }
+    });
+};
+
+var displayWeather = function(data, searchTerm) {
+    // check if api returned any repos
+    console.log(data);
+    document.getElementById("search-result-container").textContent = searchTerm;
+
+};
+
+
+
