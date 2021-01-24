0eebb8278c080789d6dbfdd0436800df3e25d9fe
Nina Cummings <jaderiver64@gmail.com>
2021-01-23 23:26:08 -0600

    more enhancements to the dashboard

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 var weatherArray = [];
 
-var weatherObj={
 
-}
 var searchBtn = document.getElementById("btn");
 
 var searchHandler = function(event) {
@@ -21,9 +19,8 @@ var getCoordinates = function(input) {
     console.log(input);
     var apiUrl = "https://api.opencagedata.com/geocode/v1/json?q=" + input + "&key=974cf3d56a9f45d58e79a7ec8b1f7842";
 
-    // make a get request to url
     fetch(apiUrl).then(function(response) {
-        // request was successful
+
         if (response.ok) {
             response.json().then(function(data) {
                 input = data.results[0].formatted;
@@ -36,15 +33,15 @@ var getCoordinates = function(input) {
 };
 
 var getWeather = function(data, searchTerm) {
-    // check if api returned any repos
+
     console.log(data);
     var latEl = data.results[0].geometry.lat;
     var lngEl = data.results[0].geometry.lng;
     var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latEl + "&lon=" + lngEl + "&appid=3812ea6836536b0581712ffd66f54fa5&units=imperial";
 
-    // make a get request to url
+
     fetch(apiUrl).then(function(response) {
-        // request was successful
+
         if (response.ok) {
             response.json().then(function(data) {
                 displayWeather(data, searchTerm);
@@ -56,15 +53,7 @@ var getWeather = function(data, searchTerm) {
 
 };
 var displayWeather = function(data, searchTerm) {
-    /*
-    searchTerm = searchTerm.charAt(0).toUpperCase() + searchTerm.slice(1);
-    var capitalizeTerms = searchTerm.split(" ");
-
-    for (var i = 0; i < capitalizeTerms.length; i++) {
-        capitalizeTerms[i] = capitalizeTerms[i][0].toUpperCase() + capitalizeTerms[i].substr(1);
-    }
-    searchTerm = capitalizeTerms.join(" ");
-    */
+    
 
     var resultMainEl = document.getElementById("search-result-container");
     resultMainEl.setAttribute("style", "border: 1px solid black; padding: 10px 10px; display: inline-block;");
@@ -79,7 +68,7 @@ var displayWeather = function(data, searchTerm) {
     imageDivEl.className = "image";
 
     var myIconEl = document.createElement('img');
-    // myIconEl.setAttribute("style","padding: 0;");
+    
 
     var iconIdEl = data.current.weather[0].icon;
     var link = "http://openweathermap.org/img/wn/" + iconIdEl + "@2x.png";
@@ -106,3 +95,4 @@ var displayWeather = function(data, searchTerm) {
     console.log(data);
 
 };
+
